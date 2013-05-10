02cfe6ec936cee426d20d1d6a73ffec25c0ccfbb
Marcel Bruch <marcel.bruch@codetrails.com>
2013-05-10 13:48:37 +0200

    Extensions points and target platform.
    Added simple text editor (more a viewer), File menu, Open menu item,
    Open handler, Target Platform

--- com.example.miniscout.client/src/com/example/miniscout/client/ApplicationWorkbenchAdvisor.java
+++ com.example.miniscout.client/src/com/example/miniscout/client/ApplicationWorkbenchAdvisor.java
@@ -6,13 +6,14 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	private static final String PERSPECTIVE_ID = "com.example.miniscout.client.perspective"; //$NON-NLS-1$
+  private static final String PERSPECTIVE_ID = "com.example.miniscout.client.perspective"; //$NON-NLS-1$
 
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new ApplicationWorkbenchWindowAdvisor(configurer);
-    }
+  public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+      IWorkbenchWindowConfigurer configurer) {
+    return new ApplicationWorkbenchWindowAdvisor(configurer);
+  }
 
-	public String getInitialWindowPerspectiveId() {
-		return PERSPECTIVE_ID;
-	}
+  public String getInitialWindowPerspectiveId() {
+    return PERSPECTIVE_ID;
+  }
 }
